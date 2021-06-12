705b6e9446fed1d163719c8d197e6b8f5e3cec60
Akash Agarwal <agwl.akash@gmail.com>
2021-06-13 04:27:15 +0530

    Bump version: 0.1.13 → 0.1.14

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ak4zh/caprover-api',
-    version='0.1.13',
+    version='0.1.14',
     zip_safe=False,
 )
