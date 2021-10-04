a0844e27e7bd86c7cdcb7a24bf159b83a1ae6925
Akash Agarwal <ak4zh@pop-os.localdomain>
2021-10-04 08:19:30 +0530

    Bump version: 0.1.19 → 0.1.20

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ak4zh/caprover-api',
-    version='0.1.19',
+    version='0.1.20',
     zip_safe=False,
 )
